5892e5bdb1fb9199d974ad985bb799d50749b4c5
James Scalise <jamescscalise@gmail.com>
2019-08-14 20:33:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 def line(deli_line)
   if deli_line.length == 0
+    puts "The line is currently empty."
   else
-    
+    line = "The line is currently:"
+    deli_line.each_with_index{|name, position|
+      line << " #{position}. #{name}"
+    }
+    return line
   end
 end
 
